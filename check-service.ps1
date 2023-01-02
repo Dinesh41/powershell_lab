3e89d615682ce9a09f44a50443eb12cace3a0d31
Dinesh <dineshsaratha8@gmail.com>
2023-01-02 11:09:37 +0530

    Add files via upload

---
+++ check-service.ps1
@@ -0,0 +1,32 @@
+﻿ 
+param(
+[Parameter( Mandatory=$true,
+            HelpMessage="Service name is needed")]
+[string]$serviceName
+)
+try{
+    $service=get-service $serviceName
+    if($service.Status -ne "Running")
+    {
+        Write-Output("Starting the service")
+        Start-Service $serviceName
+        $service=get-service $serviceName
+        if($service.Status -eq "Running"){
+            Write-Output("Service started")
+        }
+        else{
+            Write-Output("Not able to start the service")
+        }
+
+    }
+    else {
+     Write-Output("Service is already running")
+    }
+}
+catch{
+    Write-Error $_ -ErrorAction Stop
+}
+finally{
+    Write-Output("Script Ended")
+}
+
