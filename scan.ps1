14a2b65ec7c7bebc35565722c3996fffd868d755
Dinesh <dineshsaratha8@gmail.com>
2023-01-02 10:18:52 +0530

    scan for text pattern match in file

---
+++ scan.ps1
@@ -0,0 +1,12 @@
+﻿ 
+param(
+[Parameter( Mandatory=$true,
+            HelpMessage="File Path is needed")]
+[string]$path,
+
+[Parameter( Mandatory=$true,
+            HelpMessage="Pattern needed")]
+[string]$pattern
+)
+$content=get-content $path
+$content -like $pattern
